64590ab552735dea2405e6c0e6263b86154dd82e
Ralphston <ralphstonjohnson@gmail.com>
2017-05-29 17:50:07 -0500

    set site nam eon welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Di-Xperience
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Xperiences</a>
+                    <a href="https://laracasts.com">Wild Parties</a>
+                    <a href="https://laravel-news.com">Dates</a>
+                    <a href="https://forge.laravel.com">Shows</a>
+                    <a href="https://github.com/laravel/laravel">Snaps</a>
                 </div>
             </div>
         </div>
